6465a48f5e100552a808e184f794bd29c755a6c9
HarishChandra17042073 <hc82223@gmail.com>
2021-08-10 17:35:24 +0530

    Button added for functionality

---
+++ src/tutorial/useStateErrorEx.js
@@ -0,0 +1,21 @@
+import React, {useState} from 'react'
+
+function useStateErrorEx() {
+    let title="random title";
+    const handleClick= ()=> 
+{
+  console.log(title);
+}   
+ return (
+ <React.Fragment>
+      <h2>{title}</h2>
+      <button type="button" className="btn"
+      onClick={handleClick}>
+          Change title
+      </button>
+ </React.Fragment>
+       
+    );
+};
+
+export default useStateErrorEx
