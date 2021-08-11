91cd6b38da37611d9349d7af11a9ac8a2c029e9f
HarishChandra17042073 <hc82223@gmail.com>
2021-08-11 14:33:40 +0530

    render issue solved

--- src/tutorial/UseEffectBasics.js
+++ src/tutorial/UseEffectBasics.js
@@ -2,10 +2,14 @@ import React, {useState, useEffect} from 'react'
 
 function UseEffectBasics() {
     const [value,setValue] = useState(0);
+    // useEffect((value>1)); we cannot pass the condition through useEffect somehow we can use  if else statement inside the useEffect
     useEffect(()=>{
         console.log("call useEffect")
-        document.title= 'New Messages(${value}")'
-    });
+        if(value>=1){
+            document.title= `New Messages(${value}")`
+        }
+        
+    },[value]);
     console.log("Render Component")
     return (
         <>
