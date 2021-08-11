80c1a807b6eb420cce992e20c13bc743afe5ebaa
HarishChandra17042073 <hc82223@gmail.com>
2021-08-11 10:22:09 +0530

    Individual remove

--- src/tutorial/UseStateArray.js
+++ src/tutorial/UseStateArray.js
@@ -3,19 +3,26 @@ import { data } from './data'
 
 function UseStateArray() {
     const [people, setPeople] = React.useState(data) // this is how we can use useState without importing it specifically
-    return (
+    const removeItem= (id) => {
+        let newPeople= people.filter((person) => person.id !== id);
+        setPeople(newPeople);
+    }
+   return (
         <>
             {
                 people.map((person) => {
                     const {id, name}= person;
                     return(
-                        <div key={id} className ='item'>
+                        <div key={id} className ='item'> {/*className adds CSS class for styling named 'item' here */}
                             <h4>{name}</h4>
+                            <button onClick={() => removeItem(id)}>
+                                remove
+                            </button>
                         </div>
                     )
                 }
                 )}
-                <button className="btn" onClick={()=> setPeople([])}>
+                <button className="btn" onClick={()=> setPeople([])}> {/*Passing empty array to clear the data in people */}
                     clear the data
                 </button>
 
