11e6007eb9b9ca362506082a50ac0e31e5f47335
HarishChandra17042073 <hc82223@gmail.com>
2021-08-13 12:04:39 +0530

    People.js and Person.js connected

--- src/tutorial/React-Router/People.js
+++ src/tutorial/React-Router/People.js
@@ -1,20 +1,18 @@
-import React,{useState} from 'react'
-import {data} from '../data'
-import {Link} from "react-router-dom";
+import React, { useState } from 'react'
+import { data } from '../data'
+import { Link } from 'react-router-dom'
 
 function People() {
-    const [people,setPeople]=useState(data);
-        return (
+    const [people, setPeople] = useState(data)
+    return (
         <div>
-            <h1>People Page</h1>
-            {people.map((person) => {
+            <h1> People Page</h1>
+            {people.map(x => {
                 return (
-                    <div key={person.id} className="item">
-                        <h4>{person.name}</h4>
-                        <Link to={'/person/$(person.id)'}>
-                            Learn More
-                        </Link>
-                        </div>
+                    <div key={x.id} className='item'>
+                        <h4>{x.name}</h4>
+                        <Link to={`/person/${x.id}`}> Learn More</Link>
+                    </div>
                 )
             })}
         </div>
--- src/tutorial/React-Router/Person.js
+++ src/tutorial/React-Router/Person.js
@@ -1,9 +1,18 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import { data } from '../data'
+import { Link, useParams } from 'react-router-dom'
 function Person() {
+
+    const { id } = useParams()
+    const [name, setName] = useState("Default Name")
+    useEffect(() => {
+        const newName = data.find(x => x.id === parseInt(id))
+        setName(newName.name)
+    }, [])
+    console.log(id);
     return (
-        <div>
-            
+        <div><h3>Welcome {name}</h3>
+            <a href='/people'><button className='btn'>Back to People</button></a>
         </div>
     )
 }
